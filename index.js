a0746132762eda81956f702bc2f17419236ef376
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-02-20 14:37:01 -0800

    flash screen on turnstart and turnstop

--- index.js
+++ index.js
@@ -79,7 +79,9 @@ app.post('/start', function(req, res) {
 });
 
 app.post('/move', function(req, res) {
+	io.emit('game:' + game + ':turnstart', req.body.turn);
 	setTimeout(function() {
+		io.emit('game:' + game + ':turnstop', req.body.turn);
 		res.json({
 			move:  GAMES[req.body.game] || 'north',
 			taunt: _.sample(MOVE_TAUNTS)
